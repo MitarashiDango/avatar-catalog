82a2271d6d282e35c09aa53dba83811446fbadff
MitarashiDango <mitarashi_dango@mail.matcha-soft.com>
2025-05-19 19:26:39 +0900

    アバターがビルド出来ない問題を修正 (#58)

--- Runtime/AvatarMetadataSettings.cs
+++ Runtime/AvatarMetadataSettings.cs
@@ -1,10 +1,11 @@
 using UnityEngine;
+using VRC.SDKBase;
 
 namespace MitarashiDango.AvatarCatalog.Runtime
 {
     [DisallowMultipleComponent]
     [AddComponentMenu("Avatar Catalog/Avatar Metadata Settings")]
-    public class AvatarMetadataSettings : MonoBehaviour
+    public class AvatarMetadataSettings : MonoBehaviour, IEditorOnly
     {
         public AvatarMetadata avatarMetadata;
     }
